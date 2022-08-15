c7c91f880a1ed98af658968c9598265a31fcba97
tmdqls2257 <tmdqls2257@gmail.com>
2022-08-15 23:26:15 +0900

    🍱 set color

--- client/web/config/tailwind.config.js
+++ client/web/config/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors:{
+      'custom-mint': '#9DE7D7',
+      'custom-blue': '#74D1EA',
+      'custom-violet': '#BF9BDE',
+      'custom-magenta': '#F1B2DC',
+      'custom-light-grey': '#C1C6C8',
+      'custom-grey': '#A2AAAD',
+      'custom-black': '#101820'
+    }
   },
   plugins: [],
 }
