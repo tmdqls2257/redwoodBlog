4b7159fe810623d34e4610995baee8b78ce95511
tmdqls2257 <tmdqls2257@gmail.com>
2022-08-17 00:53:54 +0900

    💄 data

---
+++ client/web/src/components/Navbar/navbarData.ts
@@ -0,0 +1,30 @@
+export const navbarData = [
+  {
+    title:'mark Up',
+    path:'',
+    content:[
+    {
+      title:'HTML',
+      path:'/blog/mark_up/html',
+    },
+    {
+      title:'MarkDown',
+      path:'/blog/mark_up/mark_down',
+    },
+  ]
+  },
+  {
+    title:'Style Sheet',
+    path:'',
+    content:[
+    {
+      title:'CSS',
+      path:'/blog/style_sheet/css',
+    },
+    {
+      title:'Tailwind',
+      path:'/blog/style_sheet/tailwind',
+    },
+  ]
+  }
+]
