f467773eb6de2deec0a9cae7504fec546f3a7747
tmdqls2257 <tmdqls2257@gmail.com>
2022-08-16 21:52:55 +0900

    💄 update header's UI

--- client/web/src/components/Header/Header.tsx
+++ client/web/src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
-import { Link, routes } from "@redwoodjs/router"
+import IconBox, { IconType } from "src/shared/IconBox/IconBox"
 
-const Header = () => {
+type HeaderProps = {
+  onClick: () => void
+}
+
+const Header = ({onClick}: HeaderProps) => {
+
   return (
-    <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <header className="bg-custom-white flex items-center justify-between">
+      {/* <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -17,12 +22,17 @@ const Header = () => {
           <Link className="mr-5" to={routes.contact()}>{'Contact'}</Link>
         </nav>
         <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          {/* <Link className="mr-5" to={routes.logIn()}>{'LogIn'}</Link> */}
+          <Link className="mr-5" to={routes.logIn()}>{'LogIn'}</Link>
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
         </button>
-      </div>
+      </div> */}
+      <button onClick={onClick}>
+        <IconBox iconName={IconType.menu} />
+      </button>
+      <h5>{"teogu's tech blog"}</h5>
+      <IconBox iconName={IconType.search} />
     </header>
   )
 }
